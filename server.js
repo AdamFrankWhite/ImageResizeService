f230fe03f9c8d1b7ca26a40b112a970344a198fe
Adam White <adamfrankwhite@gmail.com>
2023-08-01 16:52:29 +0100

    local serverless test working

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 import { ApolloServer } from "@apollo/server";
 
-// The GraphQL schema
+import {
+    startServerAndCreateLambdaHandler,
+    handlers,
+} from "@as-integrations/aws-lambda";
+
 const typeDefs = `#graphql
   type Query {
     hello: String
   }
 `;
 
-// A map of functions which return data for the schema.
 const resolvers = {
     Query: {
         hello: () => "world",
     },
 };
 
-// Set up Apollo Server
 const server = new ApolloServer({
     typeDefs,
     resolvers,
 });
+
+// This final export is important!
+
+export const graphqlHandler = startServerAndCreateLambdaHandler(
+    server,
+    // We will be using the Proxy V2 handler
+    handlers.createAPIGatewayProxyEventV2RequestHandler()
+);
