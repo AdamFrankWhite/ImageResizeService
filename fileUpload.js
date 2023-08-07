3d9eae953acbcf7ad2e6616eed217d87f5c9d70b
Adam White <adamfrankwhite@gmail.com>
2023-08-07 17:48:37 +0100

    added handler function and changed port

--- fileUpload.js
+++ fileUpload.js
@@ -6,33 +6,45 @@ import * as dotenv from "dotenv";
 dotenv.config();
 // create s3 instance using S3Client
 // (this is how we create s3 instance in v3)
-const s3 = new S3Client({
-    credentials: {
-        accessKeyId: process.env.S3_ACCESS_KEY_ID, // store it in .env file to keep it safe
-        secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    },
-    region: process.env.AWS_REGION, // this is the region that you select in AWS account
-});
-const app = express();
-const port = 4000;
+module.exports.handler = async (event) => {
+    const s3 = new S3Client({
+        credentials: {
+            accessKeyId: process.env.S3_ACCESS_KEY_ID, // store it in .env file to keep it safe
+            secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+        },
+        region: process.env.AWS_REGION, // this is the region that you select in AWS account
+    });
+    const app = express();
+    const port = process.env.port || 4000;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
 
-// create memory storage object, storing image in memory
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-app.post("/upload", upload.single("image"), async (req, res) => {
-    // handle upload
-    const params = {
-        Bucket: "dino-image-library",
-        Key: req.file.originalname,
-        Body: req.file.buffer,
-        ContentType: req.file.mimetype,
-    };
+    // create memory storage object, storing image in memory
+    const storage = multer.memoryStorage();
+    const upload = multer({ storage });
+    app.post("/upload", upload.single("image"), async (req, res) => {
+        // handle upload
+        const params = {
+            Bucket: "dino-image-library",
+            Key: req.file.originalname,
+            Body: req.file.buffer,
+            ContentType: req.file.mimetype,
+        };
 
-    const command = new PutObjectCommand(params);
-    await s3.send(command);
-    res.json({ message: "File uploaded successfully!" });
-});
+        const command = new PutObjectCommand(params);
+        await s3.send(command);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(
+                {
+                    message: "File uploaded successfully!",
+                    input: event,
+                },
+                null,
+                2
+            ),
+        };
+    });
+};
