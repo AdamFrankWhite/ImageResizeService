11b93bc11db5d6dfb66d25b9cebc933023875cea
Adam White <adamfrankwhite@gmail.com>
2023-08-12 12:31:03 +0100

    added test resize code

--- resize-test.js
+++ resize-test.js
@@ -1,11 +1,27 @@
+import Jimp from "jimp";
 export const handler = async (event) => {
-    // need to get image from s3 bucket
-    // resize image
-    // return/pipe image back to user
+    let image = event.queryStringParameters;
+    let originalImage = await Jimp.read(image.imagePath);
 
+    let resizedImage = await originalImage
+        .resize(parseInt(image.imageWidth), parseInt(image.imageHeight)) // resize
+        .quality(90);
+    // .getBase64Async(Jimp.AUTO);
+    const resizedBuffer = await resizedImage.getBufferAsync(Jimp.MIME_PNG);
+    console.log(resizedBuffer.toString("base64"));
     return {
         statusCode: 200,
+        headers: {
+            "Content-Type": "image/png",
+        },
+        body: JSON.stringify(resizedBuffer.toString("base64")),
 
-        body: JSON.stringify({ message: "yo yo!", input: event }),
+        isBase64Encoded: true,
     };
+
+    // return {
+    //     statusCode: 200,
+
+    //     body: JSON.stringify({ message: "yo yo!", input: event }),
+    // };
 };
