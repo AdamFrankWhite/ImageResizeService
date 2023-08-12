850eef46956f22378b138c9fdce3511e24c68097
Adam White <adamfrankwhite@gmail.com>
2023-08-12 21:26:04 +0100

    fixed buffer/response to dynamic auto/mime type

--- resize-test.js
+++ resize-test.js
@@ -2,17 +2,16 @@ import Jimp from "jimp";
 export const handler = async (event) => {
     let image = event.queryStringParameters;
     let originalImage = await Jimp.read(image.imagePath);
-
     let resizedImage = await originalImage
         .resize(parseInt(image.imageWidth), parseInt(image.imageHeight)) // resize
         .quality(90);
     // .getBase64Async(Jimp.AUTO);
-    const resizedBuffer = await resizedImage.getBufferAsync(Jimp.MIME_PNG);
-    console.log(resizedBuffer.toString("base64"));
+    const resizedBuffer = await resizedImage.getBufferAsync(Jimp.AUTO);
+    console.log();
     return {
         statusCode: 200,
         headers: {
-            "Content-Type": "image/png",
+            "Content-Type": originalImage._originalMime,
             "Content-Encoding": "base64",
         },
         body: resizedBuffer.toString("base64"),
