a1edb2a9b507a0bedcf3f400fce559c8a49bd43c
Adam White <adamfrankwhite@gmail.com>
2023-08-15 14:32:04 +0100

    db resizeCount working, but +2 not +1

--- resize.js
+++ resize.js
@@ -1,4 +1,6 @@
 import Jimp from "jimp";
+import "dotenv/config";
+import { DynamoDBClient, UpdateItemCommand } from "@aws-sdk/client-dynamodb";
 export const handler = async (event) => {
     let image = event.queryStringParameters;
     let originalImage = await Jimp.read(image.imagePath);
@@ -7,7 +9,37 @@ export const handler = async (event) => {
         .quality(90);
     // .getBase64Async(Jimp.AUTO);
     const resizedBuffer = await resizedImage.getBufferAsync(Jimp.AUTO);
-    console.log();
+    // Define the table name
+    const tableName = "ResizeServiceTable";
+
+    // Define the partition key and sort key values
+    // const partitionKey = req.body.user; // Replace with actual partition key value
+    // Create a DynamoDB client instance
+    const dynamodbClient = new DynamoDBClient({
+        region: process.env.AWS_REGION,
+    });
+
+    // Construct the update command
+    const updateCommand = new UpdateItemCommand({
+        TableName: tableName,
+        Key: { USER: { S: "123" } },
+        ExpressionAttributeValues: { ":inc": { N: "1" } },
+        UpdateExpression: "ADD #fileResizeRequestCount :inc",
+        ExpressionAttributeNames: {
+            "#fileResizeRequestCount": "fileResizeRequestCount",
+        },
+    });
+
+    try {
+        console.log("yo");
+        const result = await dynamodbClient.send(updateCommand);
+
+        // update graphql mutation
+        console.log("Item updated successfully:", result);
+    } catch (error) {
+        console.error("Error updating item:", error);
+    }
+
     return {
         statusCode: 200,
         headers: {
