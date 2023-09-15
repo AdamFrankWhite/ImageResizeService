2443cf9ab1dc67eb17817fc7cb26dd165c0b2f37
Adam White <adamfrankwhite@gmail.com>
2023-09-15 17:03:19 +0100

    refactor

--- functions/resize.js
+++ functions/resize.js
@@ -1,54 +1,67 @@
 import Jimp from "jimp";
 import "dotenv/config";
 import { DynamoDBClient, UpdateItemCommand } from "@aws-sdk/client-dynamodb";
+import { resizeImage } from "../utils/resizeImage.js";
 export const handler = async (event) => {
     let params = event.queryStringParameters;
     let filename = params.f;
+    let externalImageURL = params.src;
     // requested dimensions
     let requestedWidth = params.w;
     let requestedHeight = params.h;
     let quality = params.q;
-
-    // measure small image first
-    let imageToRead = `https://dino-image-library.s3.eu-west-2.amazonaws.com/${filename.replace(
-        ".",
-        "_s."
-    )}`;
-    let originalImage = await Jimp.read(imageToRead);
-    // read image width
-    let smallImageWidth = originalImage.bitmap.width;
-    let smallImageHeight = originalImage.bitmap.height;
-    // calculate s3 image version to resize
-    console.log(smallImageWidth, smallImageHeight);
-
-    if (
-        requestedWidth > smallImageWidth * 5 ||
-        requestedHeight > smallImageHeight * 5
-    ) {
-        console.log("large image processed");
-        originalImage = await Jimp.read(
-            `https://dino-image-library.s3.eu-west-2.amazonaws.com/${filename}`
-        );
-    } else if (
-        requestedWidth > smallImageWidth * 2 ||
-        requestedHeight > smallImageHeight * 2
-    ) {
-        console.log("medium image processed");
-        originalImage = await Jimp.read(
-            `https://dino-image-library.s3.eu-west-2.amazonaws.com/${filename.replace(
-                ".",
-                "_m."
-            )}`
-        );
+    let imageToRead;
+    // resize external image
+    if (externalImageURL) {
+        imageToRead = externalImageURL;
     } else {
-        console.log("small image processed");
+        imageToRead = `https://dino-image-library.s3.eu-west-2.amazonaws.com/${filename.replace(
+            ".",
+            "_s."
+        )}`;
+    }
+    // measure small image first
+    let imageToResize = await Jimp.read(imageToRead);
+
+    // internal - select closest image size (s/m/l)
+    // read image width
+    if (!externalImageURL) {
+        let smallImageWidth = imageToResize.bitmap.width;
+        let smallImageHeight = imageToResize.bitmap.height;
+        // calculate s3 image version to resize
+        console.log(requestedWidth, requestedHeight);
+
+        if (
+            requestedWidth > smallImageWidth * 5 ||
+            requestedHeight > smallImageHeight * 5
+        ) {
+            console.log("large image processed");
+            imageToResize = await Jimp.read(
+                `https://dino-image-library.s3.eu-west-2.amazonaws.com/${filename}`
+            );
+        } else if (
+            requestedWidth > smallImageWidth * 2 ||
+            requestedHeight > smallImageHeight * 2
+        ) {
+            console.log("medium image processed");
+            imageToResize = await Jimp.read(
+                `https://dino-image-library.s3.eu-west-2.amazonaws.com/${filename.replace(
+                    ".",
+                    "_m."
+                )}`
+            );
+        } else {
+            console.log("small image processed");
+        }
     }
 
-    let resizedImage = await originalImage
-        .resize(parseInt(requestedWidth), parseInt(requestedHeight)) // resize
-        .quality(parseInt(quality) || 90);
+    let resizedImage = await resizeImage(
+        imageToResize,
+        parseInt(requestedWidth),
+        parseInt(requestedHeight)
+    );
     // .getBase64Async(Jimp.AUTO);
-    const resizedBuffer = await resizedImage.getBufferAsync(Jimp.AUTO);
+    // const resizedBuffer = await resizedImage.getBufferAsync(Jimp.AUTO);
     // Define the table name
     const tableName = "ResizeServiceTable";
 
@@ -72,11 +85,8 @@ export const handler = async (event) => {
     });
 
     try {
-        console.log("yo");
         const result = await dynamodbClient.send(updateCommand);
-
-        // update graphql mutation
-        console.log("Item updated successfully:", result);
+        //console.log("Item updated successfully:", result);
     } catch (error) {
         console.error("Error updating item:", error);
     }
@@ -84,10 +94,10 @@ export const handler = async (event) => {
     return {
         statusCode: 200,
         headers: {
-            "Content-Type": originalImage._originalMime,
+            "Content-Type": imageToResize._originalMime,
             "Content-Encoding": "base64",
         },
-        body: resizedBuffer.toString("base64"),
+        body: resizedImage.toString("base64"),
 
         isBase64Encoded: true,
     };
