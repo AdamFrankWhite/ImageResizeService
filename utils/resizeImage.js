88f4d65aac001f0aa2ab4276185c925d5eb34e1b
Adam White <adamfrankwhite@gmail.com>
2023-09-20 18:23:42 +0100

    add width/height  omission/auto aspect

--- utils/resizeImage.js
+++ utils/resizeImage.js
@@ -1,8 +1,16 @@
 import Jimp from "jimp";
 
 export const resizeImage = async (image, width, height) => {
+    // check for no input values
+    if (!width && !height) {
+        return image.getBufferAsync(Jimp.AUTO);
+    }
+    // provide auto width/height if only one value supplied
+    let resizeWidth = width ? width : Jimp.AUTO;
+    let resizeHeight = height ? height : Jimp.AUTO;
+    // return resized image
     let resizedImage = await image
-        .resize(parseInt(width), parseInt(height))
+        .resize(parseInt(resizeWidth), parseInt(resizeHeight))
         .quality(70);
     let imageBuffer = await resizedImage.getBufferAsync(Jimp.AUTO);
     return imageBuffer;
